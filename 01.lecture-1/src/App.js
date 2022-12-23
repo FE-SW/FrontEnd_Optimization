c5ceb46e4bebd3bf8e584c57695d5cd326132246
정석우 <ingal777@naver.com>
2022-12-24 01:56:49 +0900

    Feat: script 명령어로 text compression

--- 01.lecture-1/src/App.js
+++ 01.lecture-1/src/App.js
@@ -8,8 +8,6 @@ const ViewPage = lazy(() => import("./pages/ViewPage/index"));
 //lazy: 동적으로 해당 컴포넌트를 import함(런타임에서)
 //suspense: 컴포넌트가 동적으로 로드되므로 아무것도 로드된게 없는 시점이 존재함, 그때는 fallback을 통해 loading처리해줌
 
-
-
 function App() {
   return (
     <div className="App">
@@ -26,19 +24,33 @@ function App() {
 export default App;
 //----------------------------------------------------------------------------------------//
 
-//로딩성능: 각 리소스를 불러오는지에 대한 성능
+//<로딩성능: 각 리소스를 불러오는지에 대한 성능>
 //* 이미지 사이즈 최적화 -> 라이트하우스탭 이용
 // 1.api로 이미지를 받아올 경우 image processing CDN을 통해 해결
 // image CDN은 response로 보내기전에 특정형태로 가공작업을 한다(사이즈 줄이기,이미지 포맷 바꾸기) -> imgix 사이트 이용
 // 2.https://unsplash.com/documentation#supported-parameters 에서 제공하는 이미지 이미지 사이즈 최적화 함수 사용
 
 //* code split
+//code splitting: 덩치가 큰 번들파일을 쪼개서 작은사이즈 파일로 만드는것을 의미
+//code splitting은 페이지별로, 모듈별로 쪼갤 수 있다 -> ex)bundle.js파일을 listPage.chunk.js // viewPage.chunk.js로 쪼갤 수 있음
+
+//리액트 코드분할 페이지에서 -> router-based code splitting 사용(routing단위로 code splitting 적용)
+//https://webpack.js.org/guides/code-splitting: code splitting하는 주체가 리액트가 아닌 웹팩이기 때문에 웹팩에 코드스플리팅 관련 설정을 해줘야함(CRA는 이미 적용됨)
+
 //* 텍스트 압축
+//enable text compression: 서버로 부터 리소스를(js,css,html) 받을떄 텍스트를 압축하라는 뜻
+//네트워크탭에서 api response header을 보면 content-encoding: gzip으로 되어있다. gzip으로 인코딩 방식을 통해 압축되어있다는 뜻(일반적으로 gzip 또는 deflate방식을 이용)
+//반면에 번들링된 chunk파일의 경우 content-encoding 항목이 없는것을 확인 할  수 있다.
+//압축은 번들파일을 서빙해주는 서버에서 해줘야함
+//node ./node_modules/serve/bin/serve.js --help 명령어 치면, u옵션이 no compression이라는것을 확인할 수 있다
+//따라서 serve스크립트 명령어인 npm run build && node ./node_modules/serve/bin/serve.js -u -s build -> npm run build && node ./node_modules/serve/bin/serve.js -s build
+//파일크기가 2kb이상인 경우만 encoding 압축 해주는게 좋음
 
 //----------------------------------------------------------------------------------------
 
-//랜더링성능: 불러온 리소스를 화면에 그리는에 대한 성능 -> 퍼포먼스탭 이용
+//<랜더링성능: 불러온 리소스를 화면에 그리는에 대한 성능 -> 퍼포먼스탭 이용>
 //* bottleneck 코드 최적화
+//비효율적인 js코드 최적화
 
 //----------------------------------------------------------------------------------------//
 
@@ -53,7 +65,8 @@ export default App;
 
 // opportunites: 리소스성능과 관련된 항목(로딩) // *diagnostics: 실행성능과 관련된 항목(랜더링)
 // opportunites-minify javascript: 자바스크립트 공백,주석을 제거해서 자바스크립트 파일을 줄이라는 소리(CRA경우 production build 하면 알아서 minify를 해줌)
-// reduce javascript excution time 경우 퍼포먼스탭 이용
+// reduce javascript excution time: 퍼포먼스탭 이용
+//enable text compression: 서버로 부터 리소스를(js,css,html) 받을떄 텍스트를 압축하라는 뜻
 
 //* webpack-bundle-analyzer: 웹팩을 통해 번들링된 파일이 어떤 코드로 구성됬는지 알려줌
 //기본 webpack bundle analyzer 사용할경우 CRA를 eject를 하거나 webpack config를 custom 할 수 있는 라이브러리를 설치해야되는데 번거러운 작업임
