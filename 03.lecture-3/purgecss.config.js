b59bc5b8894e1fa48b651a35283074908749feac
jung <wjdtjrdn6@gmail.com>
2023-01-26 07:09:28 +0900

    Remove: layout shift

--- 03.lecture-3/purgecss.config.js
+++ 03.lecture-3/purgecss.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    content: ['index.html'],
-    css: ['style.css']
-  }
+  defaultExtractor: (content) => content.match(/[\w\:\-]+/g) || [], // \w = [0-9a-zA-Z_]
+};
+
+//어디까지 clssName으로 인정할 것이지 설정
