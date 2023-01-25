dbd041010a3be62959ea2e93dfa764f4c62e2c4d
jung <wjdtjrdn6@gmail.com>
2023-01-26 07:09:10 +0900

    Remove: layout shift

---
+++ 04.lecture-4/src/components/PhotoItem.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+import { showModal } from '../redux/imageModal';
+
+function PhotoItem({ photo: { urls, alt } }) {
+  const dispatch = useDispatch();
+
+  const openModal = () => {
+    dispatch(showModal({ src: urls.full, alt }));
+  };
+
+  return (
+    <ImageWrap>
+      <Image src={urls.small + '&t=' + new Date().getTime()} alt={alt} onClick={openModal} />
+    </ImageWrap>
+  );
+}
+
+const ImageWrap = styled.div`
+  width: 100%;
+  padding-bottom: 56.25%;
+  position: relative;
+`;
+
+const Image = styled.img`
+  cursor: pointer;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+export default PhotoItem;
