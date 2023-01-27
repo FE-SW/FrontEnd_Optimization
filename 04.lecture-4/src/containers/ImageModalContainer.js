4afac02cc3550b716e27a49618e689731dd4b278
jung <wjdtjrdn6@gmail.com>
2023-01-28 05:47:24 +0900

    Feat: useSelector optimization

--- 04.lecture-4/src/containers/ImageModalContainer.js
+++ 04.lecture-4/src/containers/ImageModalContainer.js
@@ -3,21 +3,22 @@ import { useSelector } from 'react-redux';
 import ImageModal from '../components/ImageModal';
 
 function ImageModalContainer() {
-  const { modalVisible, bgColor, src, alt } = useSelector(state => ({
-    modalVisible: state.imageModal.modalVisible,
-    bgColor: state.imageModal.bgColor,
-    src: state.imageModal.src,
-    alt: state.imageModal.alt,
-  }));
+  //useSelector는 리턴값기준으로 리랜더링을 결정하는데 참조형타입을 참조할시 무조건 리랜더링이 발생함
+  //해결책: 1.object를 새로만들지 않도록 state쪼개기 , 2.새로운 equality function사용
+  // const { modalVisible, bgColor, src, alt } = useSelector(state => ({
+  //   modalVisible: state.imageModal.modalVisible,
+  //   bgColor: state.imageModal.bgColor,
+  //   src: state.imageModal.src,
+  //   alt: state.imageModal.alt,
+  // }));
 
-  return (
-    <ImageModal
-      modalVisible={modalVisible}
-      bgColor={bgColor}
-      src={src}
-      alt={alt}
-    />
-  );
+  //해결책: state 쪼개기
+  const modalVisible = useSelector(state => state.ImageModal.modalVisible); //modalVisible === boolean
+  const bgColor = useSelector(state => state.ImageModal.bgColor);
+  const src = useSelector(state => state.ImageModal.src);
+  const alt = useSelector(state => state.ImageModal.alt);
+
+  return <ImageModal modalVisible={modalVisible} bgColor={bgColor} src={src} alt={alt} />;
 }
 
 export default ImageModalContainer;
--- 04.lecture-4/src/containers/PhotoListContainer.js
+++ 04.lecture-4/src/containers/PhotoListContainer.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import PhotoList from '../components/PhotoList';
 import { fetchPhotos } from '../redux/photos';
 
@@ -10,15 +10,28 @@ function PhotoListContainer() {
     dispatch(fetchPhotos());
   }, [dispatch]);
 
-  const { photos, loading } = useSelector(state => ({
-    photos:
-      state.category.category === 'all'
-        ? state.photos.data
-        : state.photos.data.filter(
-            photo => photo.category === state.category.category
-          ),
-    loading: state.photos.loading,
-  }));
+  //useSelector는 리턴값기준으로 리랜더링을 결정하는데(equality function) 참조형타입을 참조할시 무조건 리랜더링이 발생함
+  //해결책: 1.object를 새로만들지 않도록 state쪼개기 , 2.새로운 equality function사용
+  // const { photos, loading } = useSelector(state => ({
+  //   photos:
+  //     state.category.category === 'all'
+  //       ? state.photos.data
+  //       : state.photos.data.filter(photo => photo.category === state.category.category),
+  //   loading: state.photos.loading,
+  // }));
+
+  //해결책: 새로운 equality function사용 => shallowEqual함수는 참조값이 아닌 참조의 프로퍼티 하나하나를 비교해 리랜더링을 결정
+  const { category, alllPhotos, loading } = useSelector(
+    state => ({
+      category: state.category.category,
+      allPhotos: state.photos.data, //filter함수의 반환값때문에 shallowEqual를 적용해도 불필요한 리랜더링이 발생함
+      loading: state.photos.loading,
+    }),
+    shallowEqual
+  );
+
+  const photos =
+    category === 'all' ? alllPhotos : alllPhotos.filter(photo => photo.category === category);
 
   if (loading === 'error') {
     return <span>Error!</span>;
